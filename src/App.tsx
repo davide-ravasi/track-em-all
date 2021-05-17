b27da26f3201c2349e331576182707dc31f9e520
DanielLopezCS <daniellopezmail@yahoo.com>
2021-05-17 01:37:44 -0700

    Created the firebase authentication middleware as well as a minimalistic but working sign up page. Also created a minimalistic Movie add/delete page to showcase firestore capability

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import HomePage from "./pages/HomePage/HomePage";
 import AboutPage from "./pages/AboutPage/AboutPage";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import SignUp from "./components/Signup/Signup";
+import SignIn from "./components/SignIn/SignIn";
+import ExampleFirestoreUsage from "./components/ExampleFirestoreUsage/ExampleFirestoreUsage";
+
+import { AuthProvider } from "./contexts/AuthContext";
+
 import "./App.scss";
 
 function App() {
@@ -11,10 +17,19 @@ function App() {
     <div className="app">
       <Header />
       <Router>
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/about" exact component={AboutPage} />
-        </Switch>
+        <AuthProvider>
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/about" exact component={AboutPage} />
+            <Route path="/signup" exact component={SignUp} />
+            <Route path="/signin" exact component={SignIn} />
+            <Route
+              path="/ExampleFirestoreUsage"
+              exact
+              component={ExampleFirestoreUsage}
+            />
+          </Switch>
+        </AuthProvider>
       </Router>
       <Footer />
     </div>
