407deca487ef301cd3340418664095775067d538
Lal Gurung <ljgpok@yahoo.com>
2021-05-18 18:39:49 +0300

    header with simple search functionality

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,73 @@
-import React from "react";
+import React, { useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faList, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
+import './Header.scss';
+
+const people = [
+  "Siri",
+  "Alexa",
+  "Google",
+  "Facebook",
+  "Twitter",
+  "Linkedin",
+  "Sinkedin"
+];
 
 export default function Header() {
-  return <div>[Header]</div>;
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleChange = (event: any) => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    const results: any = people.filter(person =>
+      person.toLowerCase().includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+
+  return (
+    <>
+      <nav className="navbar">
+          <div className="navbar__links">
+            <div className="navbar-container--left">
+              <div className="logo">
+                <Link to="/"> Tracker
+                </Link>
+              </div>
+            </div>            
+            <div className="navbar-container--right">
+              <ul className="navbar__Item">
+                <li>
+                  <input
+                  className="navbar__menuItem"
+                  type="text"
+                  placeholder="Search Movie"
+                  value={searchTerm}
+                  onChange={handleChange}
+                  />
+                </li>
+                {searchResults.map(item => (
+                  console.log(item)
+                ))}
+                <li>
+                  <Link to="/about" >
+                  <FontAwesomeIcon className="navbar__menuItem" icon={faList} size="2x" />
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/about">
+                  <FontAwesomeIcon className="navbar__menuItem" icon={faUser} size="2x" />
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+      </nav>
+    </>
+  )
 }
