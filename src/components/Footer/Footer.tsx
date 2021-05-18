a81318634eac77f5526c64584477b837886a5098
theborgh <borghiborghi@gmail.com>
2021-05-18 11:46:18 +0200

    Add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,26 @@
 import React from "react";
-import "Footer.scss";
+import { NavLink } from "react-router-dom";
+import { ReactComponent as TmdbLogo } from "../../assets/tmdb-logo.svg";
+import "./Footer.scss";
 
 export default function Footer() {
-  return <footer className="footer">[Footer]</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer-item">
+        <NavLink to="/about" exact>
+          About us
+        </NavLink>
+      </div>
+      <div className="footer-item">
+        <a href="https://github.com/chingu-voyages/v30-bears-team-06">
+          Github repository
+        </a>{" "}
+      </div>
+      <div className="footer-item">
+        <a href="https://www.themoviedb.org/">
+          <TmdbLogo width="30px" />
+        </a>
+      </div>
+    </footer>
+  );
 }
