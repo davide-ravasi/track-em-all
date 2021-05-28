584fe690d02c2d4e68d250d923fc8cc35afe7d64
davide-ravasi <info@davideravasi.com>
2021-05-28 15:05:33 +0200

    refs #29 added hide/show button

--- src/components/ShowSeasons/ShowSeasons.tsx
+++ src/components/ShowSeasons/ShowSeasons.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleUp, faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 import ShowEpisodes from "../ShowEpisodes/ShowEpisodes";
 
@@ -11,17 +13,39 @@ type ShowSeasonsProps = {
 
 export default function ShowSeasons(props: ShowSeasonsProps) {
   const { nmbrSeasons, idShow } = props;
+  const [showBoxSeasons, setShowBoxSeasons] = useState(false);
+  const toggleSeasons = () => {
+    setShowBoxSeasons(!showBoxSeasons);
+  };
   return (
-    <div className="">
-      {nmbrSeasons &&
-        Array.from({ length: nmbrSeasons }, (value, key) => {
-          return (
-            <div className="season">
-              <h2>Season {key + 1}</h2>
-              <ShowEpisodes season={key + 1} idShow={idShow} />
-            </div>
-          );
-        })}
-    </div>
+    <>
+      <button
+        type="button"
+        className="toggleSeasons"
+        onClick={() => {
+          toggleSeasons();
+        }}
+      >
+        {showBoxSeasons ? "Hide all the episodes" : "Show all the episodes"}
+        {showBoxSeasons ? (
+          <FontAwesomeIcon icon={faAngleUp} />
+        ) : (
+          <FontAwesomeIcon icon={faAngleDown} />
+        )}
+      </button>
+      {showBoxSeasons && (
+        <div>
+          {nmbrSeasons &&
+            Array.from({ length: nmbrSeasons }, (value, key) => {
+              return (
+                <div className="season">
+                  <h2>Season {key + 1}</h2>
+                  <ShowEpisodes season={key + 1} idShow={idShow} />
+                </div>
+              );
+            })}
+        </div>
+      )}
+    </>
   );
 }
