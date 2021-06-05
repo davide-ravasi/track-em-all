fd9765e0e139ef5342ca4e4359afbf9aa1f1febf
Lal Gurung <ljgpok@yahoo.com>
2021-06-06 00:15:47 +0300

    signin and sign up page layout

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+import './SignIn.scss';
 
 export const useInput = (initialValue) => {
   const [value, setValue] = useState(initialValue);
@@ -32,39 +34,42 @@ export default function Signin() {
   }
 
   return (
-    <div>
-      <h1>Sign In</h1>
-      <form>
-        <div>
-          <label for="email"></label>
-          <input
-            type="email"
-            name="email"
-            required="required"
-            placeholder="Email Address"
-            /*{...bindEmail} */
-          ></input>
-        </div>
-        <div>
-          <label for="password" type="text"></label>
-          <input
-            type="password"
-            name="password"
-            required="required"
-            placeholder="Password"
-            /*{...bindPassword}*/
-          ></input>
-        </div>
-        <div>
-          <input
-            type="submit"
-            class="button"
-            title="Sign In"
-            value="Sign In"
-            onClick={handleSubmit}
-          ></input>
-        </div>
-      </form>
+    <div className="page">
+      <form className="login__form-container">
+       <div className="login__input-container">
+         <label htmlFor="username">Email: </label>
+         <input
+           className="login__input"
+           type="email"
+           id="username"
+           name="username"
+           required="required"
+           /*{...bindEmail} */
+         ></input>
+       </div>
+       <div className="login__input-container">
+         <label htmlFor="password">Password: </label>
+         <input
+           className="login__input"
+           type="password"
+           id="password"
+           name="password"
+           required="required"
+           /*{...bindPassword}*/
+         ></input>
+       </div>
+         <button
+           type="submit"
+           className="login__button"
+           onClick={handleSubmit}
+         >Sign In</button>
+       <div className="login__singup-text">
+         <span>Don't have account?</span>
+         <Link to="/signup">
+           <span className="login__singup-text__link">Sign Up</span>
+         </Link>
+       </div>
+     </form>
     </div>
   );
 }
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import { useHistory } from "react-router-dom";
+import { auth } from '../../firebase/firebase';
+
+import './Signup.scss';
 
 export const useInput = (initialValue) => {
   const [value, setValue] = useState(initialValue);
@@ -43,38 +46,37 @@ export default function Signup() {
   }
 
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <form>
-        <div>
-          <label for="email"></label>
+    <div className="page">
+      <form className="signup__form-container">
+        <div className="signup__input-container">
+          <label htmlFor="email">Email:</label>
           <input
             type="email"
             name="email"
+            className="signup__input"
             //required="required"
-            placeholder="Email Address"
+            // placeholder="Email Address"
             {...bindEmail}
           ></input>
         </div>
-        <div>
-          <label for="password" type="text"></label>
+        <div className="signup__input-container">
+          <label htmlFor="password" type="text">Password:</label>
           <input
             type="password"
             name="password"
+            className="signup__input"
             //required="required"
-            placeholder="Password"
+            // placeholder="Password"
             {...bindPassword}
           ></input>
         </div>
-        <div>
-          <input
+          <button
             type="submit"
-            class="button"
+            className="signup__button"
             title="Sign Up"
             value="Sign Up"
             onClick={handleSubmit}
-          ></input>
-        </div>
+          >Sign Up</button>
       </form>
     </div>
   );
