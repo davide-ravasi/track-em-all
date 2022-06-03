0971f2a2f70ab4389b3ddd237954cf1a81fef424
davide-ravasi <info@davideravasi.com>
2022-06-03 11:03:58 +0200

    fix import apollo server

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,7 +1,4 @@
-const {
-  ApolloServer,
-  gql,
-} = require("../../node_modules/apollo-server-lambda");
+const { ApolloServer, gql } = require("node_modules/apollo-server-lambda");
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
