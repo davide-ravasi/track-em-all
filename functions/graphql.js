b4b74cbd2eef6570370f5fcd52f4a02ff52a8957
davide-ravasi <info@davideravasi.com>
2022-06-06 17:21:29 +0200

    added mongodb connection favorites

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require("apollo-server-lambda");
+const mongoose = require("mongoose");
 
 const typeDefs = gql`
   type Query {
@@ -6,13 +7,27 @@ const typeDefs = gql`
   }
 `;
 
+let db;
+
 const server = new ApolloServer({
   typeDefs,
   csrfPrevention: true, // see below for more about this
+  context: async () => {
+    if (!db) {
+      try {
+        const dbClient = await mongoose.connect(process.env.MONGODB_URI, {
+          useNewUrlParser: true,
+        });
+      } catch (e) {
+        console.log("--->error while connecting with graphql context (db)", e);
+      }
+    }
+  },
   cors: {
     origin: ["http://localhost:3000"],
   },
   mocks: true,
+  introspection: true,
   playground: true, // enable GraphQL Playground IDE on prod env
 });
 
