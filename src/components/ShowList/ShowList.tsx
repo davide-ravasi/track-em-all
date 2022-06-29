708d32accf7a5964cb695d174c56ff10961f0d2c
davide-ravasi <info@davideravasi.com>
2022-06-29 22:48:02 +0200

    added paging to pagelist

--- src/components/ShowList/ShowList.tsx
+++ src/components/ShowList/ShowList.tsx
@@ -8,10 +8,31 @@ import ShowCard from "../ShowCard/ShowCard";
 import { getApiUrl } from "../../utils";
 import { en } from "../../trads/en";
 
+/*
+ * ShowList component
+ * @param {ShowListProps} props
+ * @returns {JSX.Element}
+ * @constructor
+ * @memberof ShowList
+ * @example
+ * <ShowList title="Your Favorite Shows" category="favorites" cardAmount={24} />
+ */
+
+/*
+  @todo - remove slice from map and add in the setState
+  @todo - remove results near map and add it when fetching data
+*/
+
 export default function ShowList(props: ShowListProps) {
   const [shows, setShows] = useState<{ results: Show[] } | null>();
+  const [pageNumber, setPageNumber] = useState(2);
   const defaultCardAmount = 6;
-  const { title, category, urlParameter, cardAmount = defaultCardAmount } = props;
+  const {
+    title,
+    category,
+    urlParameter,
+    cardAmount = defaultCardAmount,
+  } = props;
   const showShowMore = cardAmount === defaultCardAmount;
   const url = getApiUrl(category, urlParameter);
 
@@ -30,12 +51,41 @@ export default function ShowList(props: ShowListProps) {
     return <div>Error</div>;
   }
 
+  const handleAddCards = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    try {
+      const url = getApiUrl(category, urlParameter, pageNumber);
+      setPageNumber(pageNumber + 1);
+
+      console.log(url);
+
+      const response = await fetch(url);
+
+      const showsMore = await response.json();
+
+      setShows({ ...shows, ...showsMore.results });
+    } catch (error) {
+      console.log(error);
+    }
+
+    // if (response) {
+    //   setShows({ ...shows, results: [...shows.results, ...response.results] });
+    // }
+  };
+
   return (
     <div className="shows">
       {shows && shows?.results.length && (
         <h1>
-          {title ? title : en.categories[category].title }{" "}
-          {showShowMore && <a  className="shows__show-more" href={`/list/${category}`}>{"show more >"}</a>}
+          {title ? title : en.categories[category].title}{" "}
+          {showShowMore && (
+            <a className="shows__show-more" href={`/list/${category}`}>
+              {"show more >"}
+            </a>
+          )}
         </h1>
       )}
       <div className="shows__list">
@@ -43,6 +93,14 @@ export default function ShowList(props: ShowListProps) {
           shows?.results.slice(0, cardAmount).map((show: Show) => {
             return <ShowCard key={show.id.toString()} show={show} />;
           })}
+      </div>
+      {/* todo  add paging */}
+      <div className="shows__show-more-elements">
+        {
+          <button type="button" onClick={(e) => handleAddCards(e)}>
+            Show more
+          </button>
+        }
       </div>
     </div>
   );
