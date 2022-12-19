b6dde5ef4221b3b772ec98f35d0a522873ad953e
davide <info@davideravasi.com>
2022-12-19 16:06:17 +0000

    feat: removed express functions

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,33 +1,33 @@
-// const { ApolloServer, gql } = require("apollo-server-lambda");
-// const mongoose = require("mongoose");
+const { ApolloServer, gql } = require("apollo-server-lambda");
+const mongoose = require("mongoose");
 
-// const typeDefs = gql`
-//   type Query {
-//     hello: String
-//   }
-// `;
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-// const uri = process.env.REACT_APP_MONGODB_URI;
-// const main = async () => {
-//   await mongoose.connect(uri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   });
-// };
+const uri = process.env.REACT_APP_MONGODB_URI;
+const main = async () => {
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
 
-// main()
-//   .then(console.log("🎉 connected to database successfully"))
-//   .catch((error) => console.error(error));
+main()
+  .then(console.log("🎉 connected to database successfully"))
+  .catch((error) => console.error(error));
 
-// const server = new ApolloServer({
-//   typeDefs,
-//   csrfPrevention: true, // see below for more about this
-//   cors: {
-//     origin: "*",
-//   },
-//   mocks: true,
-//   introspection: true,
-//   playground: true, // enable GraphQL Playground IDE on prod env
-// });
+const server = new ApolloServer({
+  typeDefs,
+  csrfPrevention: true, // see below for more about this
+  cors: {
+    origin: "*",
+  },
+  mocks: true,
+  introspection: true,
+  playground: true, // enable GraphQL Playground IDE on prod env
+});
 
-// exports.handler = server.createHandler(); // Don't forget to add this!
+exports.handler = server.createHandler(); // Don't forget to add this!
