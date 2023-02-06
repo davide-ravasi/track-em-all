8b27f72b27287930b91f024e46cdfe34ce857dd1
davide <info@davideravasi.com>
2023-02-06 17:04:33 +0000

    feat: modified fns express wuth favorites get urls

--- functions/express.js
+++ functions/express.js
@@ -5,15 +5,33 @@ const cors = require('cors')
 const app = express();
 const bodyParser = require('body-parser');
 
+// connect express server to mongodb database
+const mongoose = require('mongoose');
+
+const mongoDB = process.env.REACT_APP_MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// end of mongodb connection
+
+// import models
+const { Favorite } = require('./models/favorite');
+// end of import models
+
 const router = express.Router();
-router.get('/', (req, res) => {
-  console.log("porcodio");
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+
+const getFavorites = ((req, res) => {
+  Favorite.find({}, (err, favorites) => {
+    if (err) return res.status(500).send(err);
+    console.log(favorites);
+    res.json(favorites)
+  });
 });
+
+router.get('/favorites', getFavorites);
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+// router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(cors());
 app.use(bodyParser.json());
