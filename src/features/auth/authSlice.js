f17291a1f7ebbe4dd30fa1f94a5f61a1aa5bde6f
davide-ravasi <info@davideravasi.com>
2023-03-22 07:57:10 +0100

    feat : scaffold redux and authslice

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        user: null,
+        token: null,
+    },
+    reducers: {
+        login: (state, action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        }
+    }
+});
+
+export const { login } = authSlice.actions;
