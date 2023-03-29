291ce1b1334779555c793352fb1397a66bcd8848
davide-ravasi <info@davideravasi.com>
2023-03-29 08:14:29 +0200

    feat: added base extrareducer

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,38 +1,55 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const register = createAsyncThunk("auth/register", async (data, thunkAPI) => {
+export const register = createAsyncThunk(
+  "auth/register",
+  async (data, thunkAPI) => {
     try {
-        return await fetch("http://localhost:8888/.netlify/functions/express/user/register", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: new URLSearchParams({
-                firstName: data.firstName,
-                lastName: data.lastName,
-                email: data.email,
-                password: data.password,
-            }),
-        });
+      return await fetch(
+        "http://localhost:8888/.netlify/functions/express/user/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: new URLSearchParams({
+            firstName: data.firstName,
+            lastName: data.lastName,
+            email: data.email, 
+            password: data.password,
+          }),
+        }
+      );
     } catch (error) {
-        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
 
-        return thunkAPI.rejectWithValue(message);
+      return thunkAPI.rejectWithValue(message);
     }
-});
+  }
+);
 
 export const authSlice = createSlice({
-    name: "auth",
-    initialState: {
-        user: null,
-        token: null,
+  name: "auth",
+  initialState: {
+    user: null,
+    token: null,
+  },
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
     },
-    reducers: {
-        login: (state, action) => {
-            state.user = action.payload.user;
-            state.token = action.payload.token;
-        }
-    }
+  },
+  extraReducers(builder) {
+    builder.addCase(register.fulfilled, (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    });
+  },
 });
 
 export const { login } = authSlice.actions;
