3afbd6d8cb5aa1dc4dc7ea0c98f0b7f4af2ab62d
Davide Ravasi <dravasi@d-edge.com>
2025-11-05 09:30:13 +0100

    chore: changer variables baseUrl

--- tests/home.smoke.spec.ts
+++ tests/home.smoke.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("test home page main components", async ({ page }) => {
-  await page.goto("http://localhost:5173");
+  await page.goto("/");
   await expect(page).toHaveTitle("Track'em All");
   await expect(page.getByRole("navigation")).toBeVisible();
   await expect(page.getByRole("main")).toBeVisible();
