92f9631cc943fac5b4f2b91fa082161b9abcde75
davide <info@davideravasi.com>
2025-12-05 10:51:35 +0100

    chore: cleanup reorganize package.json (#92)
    Co-authored-by: Davide Ravasi <dravasi@d-edge.com>

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
-
-// const client = new ApolloClient({
-//   uri: import.meta.env.REACT_APP_GRAPHQL_ENDPOINT,
-//   cache: new InMemoryCache(),
-// });
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
